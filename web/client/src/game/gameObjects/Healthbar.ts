44f09321f7847f2894b145a30aa9d7251566cd15
Niklas Gromann <niklasgromann@icloud.com>
2022-09-03 12:48:58 +0200

    Healthbar

---
+++ web/client/src/game/gameObjects/Healthbar.ts
@@ -0,0 +1,75 @@
+import { Application, Graphics } from "pixi.js";
+import { GridSettings } from '../../lib/GridSettings';
+import { GameObject } from "./GameObject";
+import Field from './Field';
+
+export class Healthbar extends GameObject {
+    currentHealth: number;
+    maxHealth: number;
+
+    outerHealthBarGraphics: Graphics;
+    innerHealthBarGraphics: Graphics;
+    innerHealthBarBackgroundGraphics: Graphics;
+    
+    healthBarWidth = GridSettings.tileSize * 0.8;
+    healthBarHeight = GridSettings.tileSize * 0.2;
+    healthBarPadding = GridSettings.tileSize * 0.025;
+
+    innerHealthbarWidth = this.healthBarWidth - this.healthBarPadding * 2;
+    innerHealthbarHeight = this.healthBarHeight - this.healthBarPadding * 2;
+
+    get currentInnerHealthBarWidth() {
+        return this.innerHealthbarWidth * (this.currentHealth / this.maxHealth);
+    }
+
+    constructor(app: Application, field: Field, currentHealth: number, maxHealth: number) {
+        super(app);
+
+        this.currentHealth = currentHealth;
+        this.maxHealth = maxHealth;
+
+        // Outer healthbar
+        this.outerHealthBarGraphics = new Graphics()
+            .beginFill(0xffffff)
+            .drawRect(0, 0, this.healthBarWidth, this.healthBarHeight)
+            .endFill();
+        this.outerHealthBarGraphics.zIndex = 2;
+        this.outerHealthBarGraphics.pivot.set(this.healthBarWidth / 2, this.healthBarHeight / 2);
+        
+        // Inner healthbar Background
+        this.innerHealthBarBackgroundGraphics = new Graphics()
+            .beginFill(0x464E51)
+            .drawRect(0, 0, this.currentInnerHealthBarWidth, this.innerHealthbarHeight)
+            .endFill();
+
+        this.innerHealthBarBackgroundGraphics.position.set(this.healthBarPadding, this.healthBarPadding);
+
+        this.outerHealthBarGraphics.addChild(this.innerHealthBarBackgroundGraphics);
+        
+        // Inner healthbar
+        this.innerHealthBarGraphics = new Graphics()
+            .beginFill(0xff0000)
+            .drawRect(0, 0, this.currentInnerHealthBarWidth, this.innerHealthbarHeight)
+            .endFill();
+
+        this.innerHealthBarGraphics.position.set(this.healthBarPadding, this.healthBarPadding);
+
+        this.outerHealthBarGraphics.addChild(this.innerHealthBarGraphics);
+
+        // field.container.addChild(this.outerHealthBarGraphics);
+    }
+
+    onUpdate(delta: number, deltaMs: number): void {
+    }
+
+    onDestroy(): void {
+        this.outerHealthBarGraphics.destroy();
+    }
+
+    updateHealth(currentHealth: number, maxHealth: number) {
+        this.currentHealth = currentHealth;
+        this.maxHealth = maxHealth;
+        
+        this.innerHealthBarGraphics.width = this.currentInnerHealthBarWidth;
+    }
+}
--- web/client/src/game/gameObjects/Mob.ts
+++ web/client/src/game/gameObjects/Mob.ts
@@ -1,37 +1,53 @@
-import { Application, Sprite } from "pixi.js";
+import { Application, Container, Sprite } from "pixi.js";
 import { MobModel } from "../../models";
 import { GridSettings } from '../../lib/GridSettings';
 import GridCoordinate from '../../lib/GridCoordinate';
 import { GameObject } from "./GameObject";
 import Field from './Field';
+import { Healthbar } from "./Healthbar";
 
 export class Mob extends GameObject {
     id: number;
     mobModel: MobModel;
 
+    mobContainer: Container;
     mobCircle: Sprite;
     currentCoordinate: GridCoordinate;
+
+    healthbar: Healthbar;
 
     constructor(app: Application, field: Field, mobModel: MobModel) {
         super(app);
 
         this.id = mobModel.id;
         this.mobModel = mobModel;
+        
+        this.currentCoordinate = mobModel.coordinate;
 
+        // Mob Container
+        this.mobContainer = new Container();
+
+        this.mobContainer.position.set(mobModel.coordinate.tileCenterX, mobModel.coordinate.tileCenterY);
+        this.mobContainer.zIndex = 1;
+
+        // Mob Sprite
         this.mobCircle = Sprite.from('assets/facebook_troll.jpg');
+
+        this.mobCircle.anchor.set(0.5, 0.5);
 
         this.mobCircle.width = GridSettings.tileSize * 0.7;
         this.mobCircle.height = GridSettings.tileSize * 0.7;
 
-        this.mobCircle.anchor.set(0.5, 0.5);
-        this.mobCircle.position.set(mobModel.coordinate.tileCenterX, mobModel.coordinate.tileCenterY);
-        this.mobCircle.zIndex = 1000;
+        this.mobContainer.addChild(this.mobCircle);
 
-        this.currentCoordinate = mobModel.coordinate;
+        // Healthbar
+        this.healthbar = new Healthbar(app, field, mobModel.health, mobModel.maxHealth);
+        this.healthbar.outerHealthBarGraphics.position.y = GridSettings.tileSize * -0.5;
 
-        field.container.addChild(this.mobCircle);
+        this.mobContainer.addChild(this.healthbar.outerHealthBarGraphics);
 
-        console.log(field.container.children);
+        // Others
+        field.container.addChild(this.mobContainer);
     }
 
     onUpdate(_: number, deltaMs: number): void {
@@ -39,16 +55,18 @@ export class Mob extends GameObject {
 
         this.currentCoordinate = newPosition;
 
-        this.mobCircle.position.set(newPosition.tileCenterX, newPosition.tileCenterY);
+        this.mobContainer.position.set(newPosition.tileCenterX, newPosition.tileCenterY);
     }
 
     onDestroy(): void {
-        this.mobCircle.destroy();
+        this.mobContainer.destroy();
     }
 
     updateFromModel(mobModel: MobModel) {
         this.mobModel = mobModel;
         console.log("Mob updated", mobModel);
         this.currentCoordinate = this.mobModel.coordinate;
+
+        this.healthbar.updateHealth(mobModel.health, mobModel.maxHealth);
     }
 }
